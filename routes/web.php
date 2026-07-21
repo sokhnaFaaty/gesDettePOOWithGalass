aa10ff315b86042b3f96eba38159d6006fffb41e
ngd <ndeyegalassdiagne30@gmail.com>
2026-07-21 11:14:13 +0000

    ajout des tables de routage dans routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,19 @@
 <?php
-// Table de routage simple
+// Table de routage de l'application de gestion de dettes
 return [
-    '/filieres' => ['App\Controllers\FiliereController', 'index'],
+    // Page d'accueil -> liste des clients
+    '/'                 => ['App\Controllers\UtilisateurController', 'index'],
+
+    // -------- CLIENTS (CRUD + recherche) --------
+    '/clients'          => ['App\Controllers\UtilisateurController', 'index'],
+    '/clients/create'   => ['App\Controllers\UtilisateurController', 'create'],
+    '/clients/store'    => ['App\Controllers\UtilisateurController', 'store'],
+    '/clients/edit'     => ['App\Controllers\UtilisateurController', 'edit'],
+    '/clients/update'   => ['App\Controllers\UtilisateurController', 'update'],
+    '/clients/delete'   => ['App\Controllers\UtilisateurController', 'delete'],
+
+    // -------- CONNEXION / ADMIN --------
+    '/login'            => ['App\Controllers\UtilisateurController', 'login'],
+    '/authenticate'     => ['App\Controllers\UtilisateurController', 'authenticate'],
+    '/logout'           => ['App\Controllers\UtilisateurController', 'logout'],
 ];
